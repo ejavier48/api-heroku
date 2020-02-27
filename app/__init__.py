f942f2b549bbfd9465da2b75bbece358b1ae0592
Erick Javier Sanchez Gama <esanchezg1104@alimno.ipn.mx>
2020-02-26 21:21:22 -0600

    .

---
+++ app/__init__.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+from uuid import uuid4
+import logging
+from agent import Agent, ProcessPolicy
+
+def create_app():
+    app = Flask(__name__)
+    app.secret_key =  str(uuid4())
+
+    cors = CORS(app, resources = { r"/*" : { "origins" : "*" } })
+    
+    policyFile = "policy.json"
+    proPolicy = ProcessPolicy()
+    Q = proPolicy.getPolicy(policyFile)
+
+    @app.route('/')
+    def index():
+        return 'Index Page'
+
+    @app.route("/api/agent", methods = ['POST'])
+    @cross_origin(origin = "*", headers=['Content-Type', 'Authorization'])
+    def get_simulation_data():
+        uid = uuid4()
+        logger = logging.getLogger('get_simulation_data' + str(uid))
+
+        try:
+            data = request.get_json()
+            agent = Agent(data['data'], Q)
+            response = agent.run()
+            return jsonify(response)
+        except Exception as e:
+            logger.warning('Id:{} Error: {}'.format(str(uid), e.message))
+            raise('Id:{} Error: {}'.format(str(uid), e.message))
+
+    return app
+
+if __name__ == "__main__":
+    app.run()
